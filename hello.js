e0d9bc5e3b8bf8b87f78fc60ecdcbdc292d79933
aman <118734605+idkhowtocode69420@users.noreply.github.com>
2023-04-23 09:11:26 +0200

    Add files via upload

--- hello.js
+++ hello.js
@@ -1,7 +1,7 @@
-const menu_btn = document.querySelector(".toggle");
+const toggle = document.querySelector(".toggle");
 const mobile_menu = document.querySelector(".mobile-nav");
 
-menu_btn.addEventListener("click", function () {
-  menu_btn.classList.toggle("is-active");
+toggle.addEventListener("click", function () {
+  toggle.classList.toggle("is-active");
   mobile_menu.classList.toggle("is-active");
 });
