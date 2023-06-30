0cea4f551a600ebdf894a1adb6cf8b05f5906b76
aman <118734605+amanafro@users.noreply.github.com>
2023-07-01 00:00:48 +0200

    Add files via upload

--- hello.js
+++ hello.js
@@ -16,14 +16,3 @@ function closeMenu() {
   hamburger.classList.remove("active");
   navMenu.classList.remove("active");
 }
-
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function () {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-50px";
-  }
-  prevScrollpos = currentScrollPos;
-};
